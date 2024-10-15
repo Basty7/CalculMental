e1f2b46aa0df026ec037a05bda9a214d9916eaef
ARndmPlayer <david.yuanzhi.lei@gmail.com>
2024-10-15 18:13:31 +0200

    azoduih

--- additions.js
+++ additions.js
@@ -1,2 +1,11 @@
-let x = rnd(100)
-document.write(x)
+puits = document.getElementById('inserting');
+
+function generer() {
+    for (let pas=0; pas < 10; pas++){
+        let x = rnd(100);
+        let y=rnd(100);
+        let kat = puits.appendChild(document.createElement("katex"));
+        kat.innerHTML = `${x} + ${y} `;
+    }
+    render_katex();
+}
