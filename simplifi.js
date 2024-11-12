d8e5416365afd9a661f13428377e7dcbfb0564a8
ARndmPlayer <david.yuanzhi.lei@gmail.com>
2024-11-12 17:28:25 +0100

    simplifi

--- simplifi.js
+++ simplifi.js
@@ -1,20 +1,23 @@
 let puits = document.getElementById('inserting');
 
-let asw = [
+let asw1 = [
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 ];
+
+let asw2 = [
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+];
+
 
 function generer() {
     // Vider la div puits
     puits.innerHTML = "";
     // Pour les valeurs de pas dans [0,10[ (10 valeurs)
     for (let pas = 0; pas < 10; pas++) {
-        let fx = rnd(99);
-        let x=fx+1;
-        let fy = rnd(1);
-        let y=fy+1;
-        let fz = rnd(9);
-        let z=fz+1;
+        let fx = rnd(10);
+        let x= (fx+1)**2 + rnd(2*fx)+1;
+        let y=rnd(1)+1;
+        let z=rnd(9)+1;
         let t= x*(z**(2*y));
         // Créer un div avec un katex et un input
         // document.createElement(tag) créé un nouvel élément de forme <tag></tag>
@@ -22,15 +25,23 @@ function generer() {
         let div = puits.appendChild(document.createElement("div"));
         let kat = div.appendChild(document.createElement("katex"));
         let inpt = div.appendChild(document.createElement("input"));
+        let kat2 = div.appendChild(document.createElement("katex"));
+        let inpt2 = div.appendChild(document.createElement("input"));
         // puits.appendChild(document.createElement("br"));
         // Remplir le katex
-        kat.innerHTML = `\\sqrt \{${t}\}`;
-        // Mettre le type du input à 'text' son id en fonction de son numéro et sa classe (pour le css).
+        kat.innerHTML = `\\sqrt \{${t}\} \\Large =`;
+        kat2.innerHTML = `\\sqrt{}`;
+        // Mettre le type du input à 'text' son id en fonction de son numéro et sa classe (pour le css);
         inpt.type = "text";
         inpt.id = `input${pas}`;
         inpt.class = "answers";
+
+        inpt2.type = "text";
+        inpt2.id = `inputy${pas}`;
+        inpt2.class = "answers";
         // Mettre la réponse dans la liste 
-        asw[pas] = (z**y)* x**(1/2);
+        asw1[pas] = z**y;
+        asw2[pas] = x;
     }
     // Render les maths
     render_katex();
@@ -42,8 +53,9 @@ function score() {
     for (let pas = 0; pas < 10; pas++) {
         // Récupérer l'élément avec pr id 'input0', 'input1'...
         let inpt = document.getElementById(`input${pas}`);
+        let inpt2 = document.getElementById(`inputy${pas}`)
         // Si la valeur entrée par l'utilisateur dans le input correspond à la réponse enregistrée dans asw, rajouter un point, sinon rien.
-        if (inpt.value == asw[pas]) {
+        if (inpt.value == asw1[pas] && inpt2.value == asw2[pas]) {
             score++;
         }
     }
