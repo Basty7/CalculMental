dfe7d0ad6f2ac599b36a1b6350a3d71700b3863d
ARndmPlayer <david.yuanzhi.lei@gmail.com>
2024-12-03 17:51:50 +0100

    j'arrive pas à faire insérer un symbole mathématique (comment activer un bouton en dehors de la fonction qui génère les trucs ?)

--- test2.js
+++ test2.js
@@ -1,21 +1,12 @@
-let graphe = document.getElementById('inserting');
+let puits = document.getElementById('inserting');
 
-function generer(){
-    let a = rnd(11)-5;
-    let b = rnd(11)-5;
-    let f = `a*x^2+b`;
+let div = puits.appendChild(document.CreateElement("div"));
+let kat = div.appendChild(document.createElement("katex"));
+let inpt = div.appendChild(document.createElement("input"));
+kat.innerHTML = `100`;
 
-    // On génère les valeurs
-    const xValues = [];
-    const yValues = [];
-    for (let x = 0; x <= 100; x += 0.01) {
-        yValues.push(eval(f));
-        xValues.push(x);
-    }
 
-    // Plotly pour afficher la coubre
-    const data = [{x:xValues, y:yValues, mode:"lines"}];
-    const layout = {title: "y = " + f};
-    Plotly.newPlot(graphe, data, layout);
+function racine(){
+    kat.innerHTML += `$\\sqrt{}`;
 }
 
