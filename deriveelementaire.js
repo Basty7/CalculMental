46b2b0f0157661a5bfb226985e1f86ab1412d1e4
Basty7 <bastien.boutteau@gmail.com>
2025-01-29 20:02:21 +0100

    Déso j'ai changé l'interface des polynômes... mais tout est documenté maintenant, et c'est super clean

--- deriveelementaire.js
+++ deriveelementaire.js
@@ -1,116 +1,123 @@
+/**
+ * Classe qui représente un polynôme
+ */
 class Polynome {
-    constructor(DegreeOrStringOrList) {
-        if (typeof DegreeOrStringOrList === 'number') {
-            this.list = new Array(DegreeOrStringOrList + 1).fill(0);
-            this.degree = DegreeOrStringOrList;
-            this.minimalise();
-        } else if (typeof DegreeOrStringOrList === 'string') {
-            this.list = this.fromString(DegreeOrStringOrList);
-            this.degree = this.list.length;
-            this.minimalise();
-        } else {
-            this.list = new Array(DegreeOrStringOrList.length).fill(0).map((_, i) => DegreeOrStringOrList[i] || 0);
-            this.degree = this.list.length;
-            this.minimalise();
-        }
-    }
 
-    fromString(polynomialString) {
-        // TODO: Si on a x^{34} ça fout le sbeul donc il faut regler ça
-        let Px;
-        if (polynomialString.startsWith("{") && polynomialString.endsWith("}")) {
-            Px = polynomialString.split(/\{|\}/)[1].trim(); // Extraire le polynome si il est dans {}
-        } else if (polynomialString.startsWith("(") && polynomialString.endsWith(")")) {
-            Px = polynomialString.split(/\(|\)/)[1].trim(); // Extraire le polynome si il est dans {}
-        } else {
-            Px = polynomialString.trim();
-        }
-
-        let polyList = Px.split(" + ");
-        let Coeffslist = [];
-
-        for (let monome of polyList) {
-            if (!monome.includes("x")) {
-                Coeffslist[0] = Number(monome.trim());
-            } else if (!monome.includes("x^")) {
-                Coeffslist[1] = Number(monome.split("x")[0].trim());
-            } else {
-                let coeff = Number(monome.split("x^")[0].trim()) || 1; // le ||1 permet de remplacer par 1 si la fonction Number échoue
-                let expPart = monome.split("x^")[1].trim();
-                let exp;
-                if (expPart.includes("{") && expPart.includes("}")) {
-                    exp = Number(expPart.split(/\{|\}/)[1].trim()); // Extraire l'exposant si il est dans {}
-                } else {
-                    exp = Number(expPart.trim()); // Extraire l'exposant si il n'est pas dans {}
-                }
-                Coeffslist[exp] = coeff;
-            }
-        }
-        return new Array(Coeffslist.length).fill(0).map((_, i) => Coeffslist[i] || 0);
-    }
-
-    minimalise() {
-        while (this.list.length > 1 && this.list[this.list.length - 1] === 0) {
-            this.list.pop();
-        }
-        this.degree = this.list.length - 1;
-    }
-
-    toString() {
-        return this.list
-            .map((coeff, exp) => (coeff !== 0 ? `${coeff}x^${exp}` : null))
-            .filter(term => term !== null)
-            .reverse()
-            .join(" + ");
-    }
-
-    add(other) {
-        const maxDegree = Math.max(this.list.length, other.list.length) - 1;
-        const result = new Polynome(maxDegree);
-
-        for (let i = 0; i <= maxDegree; i++) {
-            const coeff1 = this.list[i] || 0;
-            const coeff2 = other.list[i] || 0;
-            result.list[i] = coeff1 + coeff2;
-        }
-
-        return result;
-    }
-
-    derive() {
-        let derivedCoeffs = [];
-        for (let exp = 1; exp < this.list.length; exp++) {
-            if (this.list[exp] !== 0) {
-                derivedCoeffs[exp - 1] = this.list[exp] * exp;
-            }
-        }
-        const derivedPoly = new Polynome(derivedCoeffs.length - 1);
-        derivedPoly.list = derivedCoeffs;
-        return derivedPoly;
-    }
-}
-
-// Example usage:
-let poly = new Polynome("3x^2 + 2x^1 + 0");
-poly.minimalise();
-console.log(poly.toString()); // Output: "3x^2 + 2x^1"
-
-function derivepolynome(polynome) {
-	let polyList = ConvertPolynomeToList(polynome);
-	let result = []
-	console.log(polyList);
-
-	if (polyList[1] != 0 && polyList[1]) { result.push(polyList[1]) }
-	if (polyList[2] != 0 && polyList[2]) { result.push(`${2 * polyList[2]}x`) }
-	for (let exp in polyList) {
-		if (polyList[exp] * exp != 0 && exp > 2) {
-			result.push(`${polyList[exp] * exp}x^${exp - 1}`)
-			console.log("been here", exp);
+	/**
+	* Class Polynome
+	* @param {number|string|Array} DegreeOrStringOrList - Le degré du polynôme, une représentation textuelle (latex), ou une liste de ses coefficients
+	*/
+	constructor(DegreeOrStringOrList) {
+		if (typeof DegreeOrStringOrList == Number) {
+			this.list = new Array(DegreeOrStringOrList + 1).fill(0);
+			this.degree = DegreeOrStringOrList;
+			this.minimalise();
+		} else if (typeof DegreeOrStringOrList == String) {
+			this.list = this.fromString(DegreeOrStringOrList);
+			this.degree = this.list.length;
+			this.minimalise;
+		}
+		else {
+			this.list = new Array(DegreeOrStringOrList).fill(0).map((_, i) => DegreeOrStringOrList[i] || 0);
+			this.degree = this.list.length;
+			this.minimalise()
 		}
 	}
-	result.reverse()
-	return result.join(" + ");
+
+	/**
+	 * Covertit la représentation textuelle en liste
+	 * @param {string} polynomialString - La chaine de caractère représentant le polynôme
+	 * @returns {Array} La liste des coefficients
+	 */
+	fromString(polynomialString) {
+		// TODO: Si on a x^{34} ça fout le sbeul donc il faut regler ça
+		if (polynomialString.startwith("{") && polynomialString.endswith("}")) {
+			Px = polynomialString.split(/\{|\}/)[1].trim(); // Extraire le polynome si il est dans {}
+		}
+		else if (polynomialString.startwith("(") && polynomialString.endswith(")")) {
+			Px = polynomialString.split(/\(|\)/)[1].trim(); // Extraire le polynome si il est dans {}
+		}
+		else {
+			Px = polynomialString.trim();
+		}
+
+		let polyList = polynomialString.split(" + ");
+		let Coeffslist = [];
+
+		for (let monome of polyList) {
+			if (!monome.includes("x")) {
+				Coeffslist[0] = Number(monome.trim())
+			}
+			else if (!monome.includes("x^")) {
+				Coeffslist[1] = Number(monome.split("x")[0].trim())
+			}
+			else {
+				let coeff = Number(monome.split("x^")[0].trim()) || 1; // le ||1 permet de remplacer par 1 si la fonction Number échoue
+				let expPart = monome.split("x^")[1].trim();
+				let exp;
+				if (expPart.includes("{") && expPart.includes("}")) {
+					exp = Number(expPart.split(/\{|\}/)[1].trim()); // Extraire l'exposant si il est dans {}
+				} else {
+					exp = Number(expPart.trim()); // Extraire l'exposant si il n'est pas dans {}
+				}
+				Coeffslist[exp] = coeff;
+			}
+		}
+		return new Array(Coeffslist.length).fill(0).map((_, i) => Coeffslist[i] || 0);;
+	}
+
+	/**
+	 * Renvoie une représentation textuelle du polynôme
+	 * @returns {string} Une représentation textuelle du polynôme
+	 */
+	toString() {
+		return this.list.reverse().join(" + ");
+	}
+
+	/**
+	 * Additione deux polynômes
+	 * @param {Polynome} other - Le Polynôme à additioner au premier
+	 */
+	add(other) {
+		QasList = new Array(maxLength).fill(0).map((_, i) => other.list[i] || 0);
+
+		// convertir la liste des exposants en polynome
+		for (let i in PasList) {
+			this.list[i] += QasList[i];
+		}
+	}
+	
+	/**
+	 * Dérive un polynôme
+	 * @returns {Polynome} Le polynôme dérivé
+	 */
+	derive() {
+		let result = []
+
+		for (let exp in this.list) {
+			if (this.list[exp] * exp != 0) {
+				result[exp - 1] = this.list[exp] * exp;
+			}
+		}
+		const derivedPoly = new Polynome(this.list.length - 1);
+		derivedPoly.list = result;
+		return derivedPoly;
+	}
+	/**
+	 * Nettoie/minimalise le polynôme
+	 * @returns {Polynome} Le même polynôme mais nettoyé (si jamais le terme de plus haut degré a un coeff nul ou NaN)
+	 */
+	minimalise() {
+		if ((this.list[this.degree - 1] == 0 || isNaN(this.list[this.degree])) && this.degree > 0) {
+			return new Polynome(this.list.slice(-1));
+		}
+		else {
+			return this;
+		}
+	}
 }
+
+
 
 function mutipliepolynome(Px, Qx) {
 	let PasList = ConvertPolynomeToList(Px);
